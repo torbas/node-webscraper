b1d8481f6ee3c425238e4d47665562b9aeec877b
torbas <t.orbasido@gmail.com>
2015-09-01 15:30:16 -0700

    grab column text and put into csv

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
+
+var csvArr = [
+  ["Permissions", "Absolute Url", "File Type"]
+];
 
 request('http://substack.net/images/', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
     $('tr').each(function(i, element){
-      console.log($(this).text());
-      // $(this).children('td').each(function(){
-      //   cols = $(this).text().split('\n');
-      //   console.log(cols);
-        
-      // });
+      var tds = $(element).children("td");
+
+      var size = $(tds.get(1)).text();
+      var file = $(tds.get(2)).text();
+
+      if (size.length > 0) {
+        var file = $(tds.get(2)).text();
+        var ext = file.split('.')[1];
+        if(ext){
+          var permissions = $(tds.get(0)).text();
+          var fileUrl = "http://substack.net/" + $(tds.get(2)).children('a').attr('href');
+          var rowArr = [permissions, fileUrl, ext];
+          csvArr.push(rowArr);
+        }
+      };
 
     });
+
+    var file = fs.createWriteStream('substack_images.csv');
+    file.on('error', function(err) { console.log(err) });
+    csvArr.forEach(function(row) { 
+      line = row.join(',')+ '\n';
+      console.log(line);
+      file.write(line); 
+    });
+    file.end();
+
   }
 });
